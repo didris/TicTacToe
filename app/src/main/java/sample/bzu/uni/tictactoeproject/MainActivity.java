d4430e466fcfe43fc4d1b7cf69eb4cdeec69f3f2
didris <dimaid@hotmail.com>
2015-04-29 01:38:46 +0300

    Initial commit

---
+++ app/src/main/java/sample/bzu/uni/tictactoeproject/MainActivity.java
@@ -0,0 +1,163 @@
+package sample.bzu.uni.tictactoeproject;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+    boolean turn = true; // true = X & false = O
+    int turn_count = 0;
+    Button[] bArray = null;
+    Button a1, a2, a3, b1, b2, b3, c1, c2, c3;
+    TextView score;
+    int result=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        score=(TextView) findViewById(R.id.score);
+        a1 = (Button) findViewById(R.id.cell1);
+        b1 = (Button) findViewById(R.id.cell4);
+        c1 = (Button) findViewById(R.id.cell7);
+        a2 = (Button) findViewById(R.id.cell2);
+        b2 = (Button) findViewById(R.id.cell5);
+        c2 = (Button) findViewById(R.id.cell8);
+        a3 = (Button) findViewById(R.id.cell3);
+        b3 = (Button) findViewById(R.id.cell6);
+        c3 = (Button) findViewById(R.id.cell9);
+        bArray = new Button[] { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
+
+        for (Button b : bArray)
+            b.setOnClickListener(this);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_action,  menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_exit:
+                finish();
+                System.exit(0);
+                break;
+
+            case R.id.action_new:
+                turn = true;
+                turn = true;
+                turn_count = 0;
+                enableOrDisable(true);
+                score.setText(" ");
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // just to be clear. as no other views are registered YET,
+        // it is safe to assume that only
+        // the 9 buttons call this on click
+        buttonClicked(v);
+    }
+
+    private void buttonClicked(View v) {
+        Button b = (Button) v;
+        if (turn) {
+            // X's turn
+            b.setText("X");
+
+        } else {
+            // O's turn
+            b.setText("O");
+        }
+        turn_count++;
+        b.setClickable(false);
+        b.setBackgroundColor(Color.LTGRAY);
+        turn = !turn;
+
+        checkForWinner();
+    }
+
+    private void checkForWinner() {
+
+        boolean there_is_a_winner = false;
+
+        // horizontal:
+        if (a1.getText() == a2.getText() && a2.getText() == a3.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (b1.getText() == b2.getText() && b2.getText() == b3.getText()
+                && !b1.isClickable())
+            there_is_a_winner = true;
+        else if (c1.getText() == c2.getText() && c2.getText() == c3.getText()
+                && !c1.isClickable())
+            there_is_a_winner = true;
+
+            // vertical:
+        else if (a1.getText() == b1.getText() && b1.getText() == c1.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (a2.getText() == b2.getText() && b2.getText() == c2.getText()
+                && !b2.isClickable())
+            there_is_a_winner = true;
+        else if (a3.getText() == b3.getText() && b3.getText() == c3.getText()
+                && !c3.isClickable())
+            there_is_a_winner = true;
+
+            // diagonal:
+        else if (a1.getText() == b2.getText() && b2.getText() == c3.getText()
+                && !a1.isClickable())
+            there_is_a_winner = true;
+        else if (a3.getText() == b2.getText() && b2.getText() == c1.getText()
+                && !b2.isClickable())
+            there_is_a_winner = true;
+
+        if (there_is_a_winner) {
+            if (!turn)
+                message("X wins");
+            else
+                message("O wins");
+            enableOrDisable(false);
+        } else if (turn_count == 9)
+            message("No winner");
+
+    }
+
+    private void message(String text) {
+        score.setText(text);
+       // Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT)
+         //       .show();
+    }
+
+    private void enableOrDisable(boolean enable) {
+        for (Button b : bArray) {
+            b.setText("");
+            b.setClickable(enable);
+            if (enable) {
+                b.setBackgroundColor(Color.parseColor("#33b5e5"));
+            } else {
+                b.setBackgroundColor(Color.LTGRAY);
+            }
+        }
+    }
+}
